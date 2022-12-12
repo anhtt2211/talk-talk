271f0b3ceb4c090ddc57d42b1f48db11a7077964
anhtt2211 <trananh22112001@gmail.com>
2022-12-12 23:17:44 +0700

    update config and handle interactive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { Constants, MeetingProvider } from "@videosdk.live/react-sdk";
+import { SnackbarProvider } from "notistack";
+import { LeaveScreen } from "./components/screens/LeaveScreen";
+import { useTheme } from "@material-ui/styles";
+import { useMediaQuery } from "@material-ui/core";
+import { JoiningScreen } from "./components/screens/JoiningScreen";
+import { meetingTypes } from "./utils/common";
+import { MeetingContainer } from "./meeting/MeetingContainer";
+import { ILSContainer } from "./interactive-live-streaming/ILSContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [token, setToken] = useState("");
+
+  const polls = useMemo(
+    () =>
+      createdPolls.map((poll) => ({
+        ...poll,
+        isActive:
+          endedPolls.findIndex(({ pollId }) => pollId === poll.id) === -1,
+      })),
+    [createdPolls, endedPolls]
   );
-}
+
+  const useRaisedHandParticipants = () => {
+    const raisedHandsParticipantsRef = useRef();
+
+    const participantRaisedHand = (participantId) => {
+      const raisedHandsParticipants = [...raisedHandsParticipantsRef.current];
+
+      const newItem = { participantId, raisedHandOn: new Date().getTime() };
+
+      const participantFound = raisedHandsParticipants.findIndex(
+        ({ participantId: pID }) => pID === participantId
+      );
+
+      if (participantFound === -1) {
+        raisedHandsParticipants.push(newItem);
+      } else {
+        raisedHandsParticipants[participantFound] = newItem;
+      }
+
+      setRaisedHandsParticipants(raisedHandsParticipants);
+    };
+
+    useEffect(() => {
+      raisedHandsParticipantsRef.current = raisedHandsParticipants;
+    }, [raisedHandsParticipants]);
+
+    const _handleRemoveOld = () => {
+      const raisedHandsParticipants = [...raisedHandsParticipantsRef.current];
+
+      const now = new Date().getTime();
+
+      const persisted = raisedHandsParticipants.filter(({ raisedHandOn }) => {
+        return parseInt(raisedHandOn) + 15000 > parseInt(now);
+      });
+
+      if (raisedHandsParticipants.length !== persisted.length) {
+        setRaisedHandsParticipants(persisted);
+      }
+    };
+
+    useEffect(() => {
+      const interval = setInterval(_handleRemoveOld, 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }, []);
+
+    return { participantRaisedHand };
+  };
+
+  const theme = useTheme();
+  const isXStoSM = useMediaQuery(theme.breakpoints.only("xs"));
+
+  useEffect(() => {
+    if (isXStoSM) {
+      window.onbeforeunload = () => {
+        return "Are you sure you want to exit?";
+      };
+    }
+  }, [isXStoSM]);
+
+  return (
+    <>
+      {isMeetingStarted ? (
+        <SnackbarProvider
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          autoHideDuration={5000}
+          maxSnack={3}
+        >
+          <MeetingProvider
+            config={{
+              meetingId,
+              micEnabled: micOn,
+              webcamEnabled: webcamOn,
+              name: participantName ? participantName : "TestUser",
+              mode: meetingMode,
+              multiStream: meetingType === meetingTypes.MEETING ? true : false,
+            }}
+            token={token}
+            reinitialiseMeetingOnConfigChange={true}
+            joinWithoutUserInteraction={true}
+          >
+            {meetingType === meetingTypes.MEETING ? (
+              <MeetingContainer />
+            ) : (
+              <ILSContainer />
+            )}
+          </MeetingProvider>
+        </SnackbarProvider>
+      ) : isMeetingLeft ? (
+        <LeaveScreen setIsMeetingLeft={setIsMeetingLeft} />
+      ) : (
+        <JoiningScreen />
+      )}
+    </>
+  );
+};
 
 export default App;
